a831f2cfa350d04c43bf81dc74aad6dca4355e05
OsamaRafayia <90762009+OsamaRafayia@users.noreply.github.com>
2021-10-07 00:27:25 +0300

    Update save.js

--- commands/save.js
+++ commands/save.js
@@ -1,4 +1,3 @@
-const { splitBar } = require("string-progressbar");
 const { MessageEmbed } = require("discord.js");
 const i18n = require("../util/i18n");
 
@@ -6,18 +5,16 @@ module.exports = {
 name: "save",
 description: i18n.__("save.description"),
 execute(message) {
-    const queue = message.client.queue.get(message.guild.id);
-  if (!queue) return message.reply(i18n.__("save.errorNotQueue")).catch(console.error);
+  const queue = message.client.queue.get(message.guild.id);
+if (!queue) return message.reply(i18n.__("save.errorNotQueue")).catch(console.error);
 
   const song = queue.songs[0];
-  const seek = (queue.connection.dispatcher.streamTime - queue.connection.dispatcher.pausedTime) / 1000;
-  const left = song.duration - seek;
   let save = new MessageEmbed()
-    .setTitle(i18n.__("save.embedTitle"))
-    .setDescription(`${song.title}\n${song.url}`)
-    .setColor("#f1c40f")
-    .setAuthor(message.client.user.username);
+  .setTitle(i18n.__("save.embedTitle"))
+  .setDescription(`${song.title}\n${song.url}`)
+  .setColor("#f1c40f")
+  .setAuthor(message.client.user.username);
 
-    return message.author.send(save);
+  return message.author.send(save).catch(() => message.channel.send(i18n.__mf("save.errorDmsGif", { author: message.author })));
 }
 };
